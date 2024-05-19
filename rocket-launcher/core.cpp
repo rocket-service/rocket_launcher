164d3ebc1dd58591e8e57e96a620f5a05c5a6e3a
Joshua <69308011+Ke4ton@users.noreply.github.com>
2024-05-19 23:49:51 +0300

    added UServer to thirdparty

--- rocket-launcher/core.cpp
+++ rocket-launcher/core.cpp
@@ -1,6 +1,19 @@
+#include "web_socket/web_socket.h"
+
+#ifdef USE_GUI
 
 int main( )
 {
 
 	return 0;
 }
+
+#else
+
+int WinMain( HINSTANCE hInstance, HINSTANCE, LPSTR, int nShowCmd )
+{
+
+	return 0;
+}
+
+#endif
